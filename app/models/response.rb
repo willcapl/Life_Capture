990b3e8163ddffe8cafd7d6160ffc4b11dbc46b7
Digby Hebb <digby.hebb@gmail.com>
2023-11-27 16:49:29 +0000

    associations half done

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,3 +1,4 @@
 class Response < ApplicationRecord
   belongs_to :question
+  belongs_to :sub_user, through: :question
 end
--- app/models/sub_user.rb
+++ app/models/sub_user.rb
@@ -1,3 +1,5 @@
 class SubUser < ApplicationRecord
+  has_many :questions
+  has_many :responses, through: :questions
   belongs_to :user
 end
