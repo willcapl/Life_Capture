1394e4f28559e77c7db2c9785e517354e160ccb1
Digby Hebb <digby.hebb@gmail.com>
2023-11-28 10:55:27 +0000

    added all routes within spreadsheets

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  resources :sub_users, only: [:new, :edit, :update, :create, :destroy, :show] do
+    resources :questions, only: [:create] do
+      resources :responses, only: [:create]
+    end
+  end
+  get 'dashboard', to: 'pages#dashboard'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
