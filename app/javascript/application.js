60d7a6192d12559481c7fded193682334e07f4e1
will caplan <willcaplan9@gmail.com>
2023-11-29 17:01:12 +0000

    added

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,5 +3,3 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
-import QuestionButtonController from "./controllers/question_button_controller.js"
-Stimulus.register("next-button", QuestionButtonController)
