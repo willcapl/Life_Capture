27e9c595253f41c82b25c288d38a470525a02f4f
alex morelli <morellialexander1@gmail.com>
2023-11-29 14:54:48 +0000

    added

--- db/migrate/20231127160422_create_responses.rb
+++
@@ -1,9 +0,0 @@
-class CreateResponses < ActiveRecord::Migration[7.1]
-  def change
-    create_table :responses do |t|
-      t.references :question, null: false, foreign_key: true
-
-      t.timestamps
-    end
-  end
-end
