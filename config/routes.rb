e30b929677d8adada340e8f1f94cd8a27c0e2eeb
will caplan <willcaplan9@gmail.com>
2023-11-30 13:27:32 +0000

    added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
   root to: "pages#home"
   resources :sub_users, only: [:new, :edit, :update, :create, :destroy], except: [:show] do
     resources :questions, only: [:index, :show, :update]
@@ -15,4 +15,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get 'FAQ', to: 'pages#faq'
+
+  get 'sub_user_selection', to: 'pages#sub_user_selection'
+
+  get 'others_new', to: 'sub_users#other_new'
 end
