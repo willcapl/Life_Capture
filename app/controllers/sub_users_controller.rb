21167874e1a1e6b4ebd7650d94e934d86d4765eb
Cecilia Cagnetta <mariaceciliacagnetta@gmail.com>
2023-11-30 15:40:45 +0000

    added

--- app/controllers/sub_users_controller.rb
+++ app/controllers/sub_users_controller.rb
@@ -11,6 +11,7 @@ class SubUsersController < ApplicationController
     @sub_user = SubUser.new(sub_user_params)
     @sub_user.user = current_user
     if @sub_user.save
+      @sub_user.content
       redirect_to @sub_user
     else
       render :new, status: :unprocessable_entity
