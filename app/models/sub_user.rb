761c170524ff58072e6365c7ba78c4f6615365c3
Cecilia Cagnetta <mariaceciliacagnetta@gmail.com>
2023-11-30 16:20:31 +0000

    added

--- app/models/sub_user.rb
+++ app/models/sub_user.rb
@@ -9,7 +9,7 @@ class SubUser < ApplicationRecord
     def content
       client = OpenAI::Client.new
       chaptgpt_response = client.chat(parameters: {
-        model: "gpt-4-1106-preview",
+        model: "gpt-3.5-turbo",
         messages: [{ role: "user", content: "Don't list the questions by number, just type | between each one. Your role is to engage elderly individuals in meaningful conversations by asking them a series of 14 questions. These questions will range from their childhood memories to their vocational challenges and family life. Only give the questions as output. These are the user responses #{self.name}, #{self.relationship_to_user}, #{self.dob}, #{self.childhood_location}, #{self.post_education}, #{self.birthplace}, #{self.career}, #{self.adult_life_location}, #{self.hobbies}, #{self.life_after_retirement}. Don't list the questions by number, just type | between each one}"}]
       })
       questions = chaptgpt_response["choices"][0]["message"]["content"]
