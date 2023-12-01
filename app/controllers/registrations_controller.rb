e25735f6afcd453ac92d9e2afb15a0e78fac4a95
will caplan <willcaplan9@gmail.com>
2023-12-01 12:01:33 +0000

    added

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    sub_user_selection_path # Or :prefix_to_your_route
+    sub_user_selection_path
   end
 end
