ca14069a6c600b06663a3784dc8b2e32aa4499b9
will caplan <willcaplan9@gmail.com>
2023-12-06 12:24:37 +0000

    added some changes

--- app/models/sub_user.rb
+++ app/models/sub_user.rb
@@ -9,18 +9,20 @@ class SubUser < ApplicationRecord
   #   :hobbies, :life_after_retirement, presence: true
 
   def content
-    client = OpenAI::Client.new
-    chaptgpt_response = client.chat(parameters: {
-      model: "gpt-4",
-      messages: [{ role: "user", content: "Don't list the questions by number, just type | between each one. Your role
-        is to engage elderly individuals in meaningful conversations by asking them a series of 14 questions. These questions
-        will range from their childhood memories to their vocational challenges and family life. Only give the questions as output.
-        These are the user responses, the user's name is #{self.name}, they were born on #{self.dob}, they spent their childhood
-        in #{self.childhood_location}, they were primarilary educated at #{self.education}, their career was
-        #{self.career}, they spent their adult life in #{self.adult_life_location}, their favourite hobbies or passions are #{self.hobbies}. Don't list the questions by number, just type | between each one.}. Also insert questions about their first job and their parents. Make the questions highly specific and ask
-        about friends."}]
-    })
-    questions = chaptgpt_response["choices"][0]["message"]["content"]
+    # client = OpenAI::Client.new
+    # chaptgpt_response = client.chat(parameters: {
+    #   model: "gpt-4",
+    #   messages: [{ role: "user", content: "Don't list the questions by number, just type | between each one. Your role
+    #     is to engage elderly individuals in meaningful conversations by asking them a series of 14 questions. These questions
+    #     will range from their childhood memories to their vocational challenges and family life. Only give the questions as output.
+    #     These are the user responses, the user's name is #{self.name}, they were born on #{self.dob}, they spent their childhood
+    #     in #{self.childhood_location}, they were primarilary educated at #{self.education}, their career was
+    #     #{self.career}, they spent their adult life in #{self.adult_life_location}, their favourite hobbies or passions are #{self.hobbies}. Don't list the questions by number, just type | between each one.}. Also insert questions about their first job and their parents. Make the questions highly specific and ask
+    #     about friends."}]
+    # })
+    # questions = chaptgpt_response["choices"][0]["message"]["content"]
+    questions = "Tell me about the reasons you went into Dentistry? | What was it like growing up during the war? | How did Dentistry change in your long career?
+      |"
     questions_array = questions.split("|")
     questions_array.each_with_index do |title, index|
       date = Date.today
